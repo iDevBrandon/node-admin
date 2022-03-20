796d68a13930f7a148db1a9b1c209d871945f453
Brandon Ha <idevbrandon@gmail.com>
2022-03-20 15:32:56 +0900

    Set routes for APIs

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Admin page study
 
 ## 3. TypeScript
-
 Simply apply typescript into project
 
 ## 4. Express
-
 Set up a simple express server
+
+## 5. Routes
+In controller, create all relavent routes/functions
+In routes.ts, put all of route APIs
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-let port = process.env.PORT || 8000;
+import { routes } from "./routes";
 
+const port = process.env.PORT || 8000;
 const app = express();
-app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world");
-});
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+
+routes(app);
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello world");
+// });
 
 app.listen(port, () => {
   console.log("listening on port " + port);
