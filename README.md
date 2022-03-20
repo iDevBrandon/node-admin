34f1728f26ad5d244cc7da829470228e2c0018cf
Brandon Ha <idevbrandon@gmail.com>
2022-03-20 14:01:40 +0900

    Set up an express server

---
+++ README.md
@@ -0,0 +1,9 @@
+# Admin page study
+
+## 3. TypeScript
+
+Simply apply typescript into project
+
+## 4. Express
+
+Set up a simple express server
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hi");
+import express, { Request, Response } from "express";
+import cors from "cors";
+let port = process.env.PORT || 8000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
