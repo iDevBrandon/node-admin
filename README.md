174bd47e6396ece84f2041c33702a961cc576a05
Brandon Ha <idevbrandon@gmail.com>
2022-03-20 16:07:44 +0900

    6. validation

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Set up a simple express server
 ## 5. Routes
 In controller, create all relavent routes/functions
 In routes.ts, put all of route APIs
+
+## 6. validation
+To registeration for validation, set the rules in register.validation.ts
+and then use it in register() 
---
+++ src/validation/register.validation.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { Joi } from "express-validation";
+
+export const RegisterValidation = Joi.object({
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  password_confirm: Joi.string().required(),
+});
